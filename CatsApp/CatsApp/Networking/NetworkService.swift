76afe8dbeb4bbf886803f67f0d8d6084c4c7d7aa
yaprincev <5489812@mail.ru>
2024-04-11 12:56:12 +0300

    add api key

--- CatsApp/CatsApp/Networking/NetworkService.swift
+++ CatsApp/CatsApp/Networking/NetworkService.swift
@@ -21,10 +21,11 @@ class NetworkService: NetworkServiceProtocol {
     let sessionConfiguration = URLSessionConfiguration.default
     let decoder = JSONDecoder()
     let session = URLSession.shared
+    let apiKey = "live_zj8k3tkUBRYHpoByq7RTmV0NBz8moVcujbGMA0DVyN4FJtxheqbH1Uuv3DF9cOB3"
     
     
     func getCats(completion: @escaping (Result<[Cat]?, Error>) -> Void) {
-        guard let url = URL(string: "https://api.thecatapi.com/v1/images/search?limit=30") else { return }
+        guard let url = URL(string: "https://api.thecatapi.com/v1/images/search?limit=10&has_breeds=1&api_key=\(apiKey)") else { return }
         session.dataTask(with: url) { data, response, error in
             if let error = error {
                 completion(.failure(error))
@@ -52,8 +53,7 @@ class NetworkService: NetworkServiceProtocol {
     }
     
     func getCatInfo(for id: String, completion: @escaping (Result<CatInfo?, Error>) -> Void) {
-        guard let url = URL(string: "https://api.thecatapi.com/v1/images/0XYvRd7oD") else { return }
-        //guard let url = URL(string: "https://api.thecatapi.com/v1/images/\(id)") else { return }
+        guard let url = URL(string: "https://api.thecatapi.com/v1/images/\(id)") else { return }
         session.dataTask(with: url) { data, response, error in
             if let error = error {
                 completion(.failure(error))
